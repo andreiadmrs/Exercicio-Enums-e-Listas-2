6e3dba645c4e1ce3f36c75a5e995cef753130b32
Andreia de Moraes <andreiadmrs@gmail.com>
2020-09-08 12:23:59 -0300

    Exercício do curso de C#. Enums e Listas.

---
+++ Enum3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Enum3.Entities;
+using Enum3.Entities.Enums;
+
+namespace Enum3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("E-mail:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Birth date (DD/MM/YYYY):");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.WriteLine("Order status:");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Console.WriteLine("How many items to this order?");
+            int q = int.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthdate);
+            Order order = new Order(DateTime.Now, status,client);
+            
+
+
+            for (int i = 1; i <= q; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.WriteLine("Product name:");
+                string productName = Console.ReadLine();
+                Console.WriteLine("Product price:");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Product quantity:");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(productName, productPrice);
+                OrderItem item = new OrderItem(productQuantity, productPrice, product);
+                order.AddItem(item);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+        }
+    }
+}
